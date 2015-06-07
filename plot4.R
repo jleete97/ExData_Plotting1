bcf6ebe9717396ab860cbdc522f1ca877495ac7e
Jon Leete <jleete97@gmail.com>
2015-06-07 02:12:15 -0400

    finished plot 4

--- plot4.R
+++ plot4.R
@@ -14,5 +14,15 @@ date_filter <- seq(as.Date("2007-02-01"), to = as.Date("2007-02-02"), by = "1 da
 # Generate multi-panel plot to file
 png(filename = "plot4.png", height = 480, width = 480)
 
+par(mfrow = c(2, 2))
+
+with(subset(p, date_only %in% date_filter),
+{ plot(date_time, Global_active_power, type="l", xlab="", ylab="Global Active Power")
+  plot(date_time, Voltage, type="l", xlab="datetime")
+  plot(date_time, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+    lines(date_time, Sub_metering_2, col="red")
+    lines(date_time, Sub_metering_3, col="blue")
+    legend("topright", lwd=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n")
+  plot(date_time, Global_reactive_power, type="l", xlab="datetime") })
 
 dev.off()
